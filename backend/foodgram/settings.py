021fe2c17ef77f457efa435f920d5d8090551d81
Mia Evans <miaevansla@gmail.com>
2022-05-23 20:24:41 +0300

    update

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -60,14 +60,21 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get('ENGINE', default='django.db.backends.postgresql'),
-        'NAME': os.environ.get('DB_NAME', default='postgres'),
-        'USER': os.environ.get('POSTGRES_USER', default='postgres'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', default='postgres'),
-        'HOST': os.environ.get('DB_HOST', default='db'),
-        'PORT': os.environ.get('DB_PORT', default=5432)
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+    #'default': {
+        #'ENGINE': os.environ.get('ENGINE', default='django.db.backends.postgresql'),
+        #'NAME': os.environ.get('DB_NAME', default='postgres'),
+        #'USER': os.environ.get('POSTGRES_USER', default='postgres'),
+        #'PASSWORD': os.environ.get('POSTGRES_PASSWORD', default='postgres'),
+        #'HOST': os.environ.get('DB_HOST', default='db'),
+        #'PORT': os.environ.get('DB_PORT', default=5432)
+    #}
+#}
 
 
 AUTH_PASSWORD_VALIDATORS = [
