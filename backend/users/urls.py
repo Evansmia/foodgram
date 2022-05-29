1af6c2ba1bfabbf31fce62265ba960749ab1a846
Mia Evans <miaevansla@gmail.com>
2022-05-29 18:30:52 +0300

    update

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -4,9 +4,10 @@ from djoser import views
 from .views import UserViewSet
 
 urlpatterns = [
-    path('users/<int:id>/subscribe/', UserViewSet.as_view(),
+    path('users/<int:id>/subscribe/', UserViewSet.as_view(
+         {'post': 'create', 'delete': 'destroy'}),
          name='subscribe'),
-    path('users/subscriptions/', UserViewSet.as_view(),
+    path('users/subscriptions/', UserViewSet.as_view({'get': 'list'}),
          name='subscription'),
     path('auth/token/login/', views.TokenCreateView.as_view(), name='login'),
     path('auth/token/logout/', views.TokenDestroyView.as_view(),
