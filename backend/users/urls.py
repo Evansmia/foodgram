71987056b1af3949a37648fcbc924e15d2728ff7
Mia Evans <miaevansla@gmail.com>
2022-05-29 16:21:10 +0300

    update

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,15 +1,14 @@
-from django.urls import include, path
-from djoser import views
+from django.urls import include, path, re_path
+from rest_framework.routers import DefaultRouter
 
-from users.views import FollowView, FollowListViewSet
+from .views import UserViewSet
+
+app_name = 'users'
+
+router = DefaultRouter()
+router.register('users', UserViewSet)
 
 urlpatterns = [
-    path('users/<int:id>/subscribe/', FollowView.as_view(),
-         name='subscribe'),
-    path('users/subscriptions/', FollowListViewSet.as_view(),
-         name='subscription'),
-    path('auth/token/login/', views.TokenCreateView.as_view(), name='login'),
-    path('auth/token/logout/', views.TokenDestroyView.as_view(),
-         name='logout'),
-    path('', include('djoser.urls')),
+    path(r'', include(router.urls)),
+    re_path(r'auth/', include('djoser.urls.authtoken')),
 ]
