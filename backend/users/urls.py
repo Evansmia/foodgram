49e9de2a099582ac8f9510250008e0fe207f5d61
Mia Evans <miaevansla@gmail.com>
2022-05-29 17:10:54 +0300

    update

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import include, path
+from djoser import views
 from rest_framework.routers import DefaultRouter
 
 from .views import UserViewSet
@@ -6,9 +7,12 @@ from .views import UserViewSet
 app_name = 'users'
 
 router = DefaultRouter()
-router.register('users', UserViewSet)
+router.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('auth/', include('djoser.urls.authtoken')),
+    path('', include('djoser.urls')),
+    path('auth/token/login/', views.TokenCreateView.as_view(), name='login'),
+    path('auth/token/logout/', views.TokenDestroyView.as_view(),
+         name='logout'),
+    path('', include(router.urls))
 ]
